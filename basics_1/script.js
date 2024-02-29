f421042fd9881e5e60272e74ca5bfc6db6f8b2e0
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2024-02-29 17:18:21 +0000

    container config done

--- basics_1/script.js
+++ basics_1/script.js
@@ -1,1 +1,32 @@
-console.log("huihuihui");
+
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof Boolean);
+// console.log(typeof Function);
+// console.log(typeof String);
+// console.log(typeof Object);
+
+// -------------------Memory allocation in stack-----------
+// let add1 = "Manmeet";
+// let add2 = add1;
+// add1 = "Panwar";
+// console.log(add2);
+// console.log(add1);
+
+// -------------------Memory allocation in Heap------------
+
+// let user1 = {
+//     name : "manie",
+//     gmail : "manmeetpanwar567@gmail.com"
+// }
+
+// console.log(user1.name);
+
+// let user2 = user1;
+// user2.gmail = "kadduLassan@google.com"
+
+// console.log(user2.gmail)
+// console.log(user1.gmail)
+
+
+
---
+++ basics_1/string.js
@@ -0,0 +1,4 @@
+// const name = new String("Manmeet");
+const game = "Kaddu";
+const fame = "Lassan";
+console.log(`game is ${game.toUpperCase()} and fame is ${fame.toLowerCase()}`);
