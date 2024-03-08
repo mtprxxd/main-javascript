ecd9f75e7d519b8dc22f72c827bc4bdd7958fd1c
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2024-03-08 17:50:17 +0000

    container config done

--- basics_1/string.js
+++ basics_1/string.js
@@ -1,4 +1,26 @@
 // const name = new String("Manmeet");
-const game = "Kaddu";
-const fame = "Lassan";
-console.log(`game is ${game.toUpperCase()} and fame is ${fame.toLowerCase()}`);
+// const game = "Kaddu";
+// const fame = "Lassan";
+// console.log(`game is ${game.toUpperCase()} and fame is ${fame.toLowerCase()}`);
+// console.log(name);
+
+// alert(3+3);
+// let score = true;
+// let value = Number(score);
+// console.log(value);
+// let a = 1;
+// let b = Number(a);
+// console.log("2">="b");
+
+//  num_int = 123;  
+//  num_float = 1.23;  
+
+// total = num_int + num_float;
+// console.log(total);
+
+// let string = "123";
+// new_string = Number(string);
+// console.log(new_string);
+
+
+
