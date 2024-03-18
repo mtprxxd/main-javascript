dc72df60706e8c32f44360387901b41570308e7c
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2024-03-18 16:57:25 +0000

    container config done

---
+++ basics_1/number.js
@@ -0,0 +1,14 @@
+// Numbers
+// let score = 100;
+// console.log(score);
+
+let tata = new Number(10000);
+console.log(tata);
+
+let bye = new Number(100.786);
+console.log(tata.toFixed(4));
+
+console.log(tata.toLocaleString());
+console.log(bye.toPrecision(5));
+console.log(tata.toString(2));
+// console.log(typeof tata);
