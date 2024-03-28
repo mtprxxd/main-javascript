1a58100ec9d8aa56bc1acd3dbcb5faf0f46453c9
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2024-03-28 16:37:41 +0000

    container config done

---
+++ basics_1/date_time.js
@@ -0,0 +1,28 @@
+const newDate = new Date();
+// console.log(newDate);
+console.log(newDate.toString());
+// console.log(newDate.toDateString());
+// console.log(newDate.toTimeString());
+// console.log(newDate.toJSON());
+// console.log(newDate.toISOString());
+// console.log(newDate.toLocaleDateString());
+// console.log(newDate.toLocaleString());
+
+const createDate = new Date(2024,1,10,10,55,20);
+// console.log(createDate.toLocaleString());
+
+// let date = Date.now();
+// console.log(date);
+// console.log(createDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDash = new Date();
+// console.log(newDash.getHours());
+console.log(newDash.getDay());
+// console.log(newDash.getMonth());
+// console.log(`Month is ${newDash.getMonth()}, Day is ${newDash.getDay()} and the time is ${newDash.getHours()}:${newDash.getMinutes()}:${newDash.getSeconds()}`);
+// console.log(`Month is ${newDash.getMonth()}, Day is ${newDash.toDateString()} and the time is ${newDash.getHours()}:${newDash.getMinutes()}:${newDash.getSeconds()}`);
+
+newDash.toLocaleString('default',{
+    weekday: 'long'
+});
