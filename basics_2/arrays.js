e7f5d4c9a6ed8b07fb4c184d15eaf3d2ff4b2bdc
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2024-03-30 14:55:22 +0000

    container config done

--- basics_2/arrays.js
+++ basics_2/arrays.js
@@ -1,22 +1,54 @@
 // Arrays
 let orgArr = [1,2,3,4,5,6];
-console.log(orgArr);
+// console.log(orgArr);
 
-let newArr = new Array(1,2,3,4,5,6);
-console.log(newArr[1]);
+// let newArr = new Array(1,2,3,4,5,6);
+// console.log(newArr[1]);
 
 // -------------------------------------------------------
 // Methods
-orgArr.push(7);
+// orgArr.push(7);
 // orgArr.pop();
 
 // orgArr.unshift(1);
 // orgArr.shift();
-console.log(orgArr);
+// console.log(orgArr);
 
-console.log(orgArr.includes(7));
+// console.log(orgArr.includes(7));
 
-const nArr = orgArr.join();
-console.log(nArr);
+// const nArr = orgArr.join();
+// console.log(nArr);
 
-console.log(typeof nArr);
+// console.log(typeof nArr);
+
+
+// console.log("A",orgArr);
+// console.log(orgArr.slice(0,3)); ----in slice the data remains same in array---
+
+
+// console.log("B",orgArr);
+// console.log(orgArr.splice(0,3));---but in splice data deleted permanently----
+
+// console.log(orgArr);
+
+// --------------------------------------------------------------------------------------------------------
+// Methods 2.0
+// const oggy = ["oggy", "jack","olivia","bob"];
+// const doraemon = ["nobita","sizuka","gian","sunio"];
+
+// const newArr = doraemon.join();
+// const newArr = oggy.concat(doraemon);
+// console.log(typeof newArr);
+// console.log(newArr);
+
+
+// console.log(Array.isArray("Manmeet"));
+// console.log(Array.from("Manmeet"));
+
+
+// let a = 1
+// let b = 2
+// let c = 3
+// console.log(Array.of(a,b,c));
+
+
