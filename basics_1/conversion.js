2427371da27d933f8e10967e52d2d3fd229be8b5
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2025-02-05 18:31:37 +0000

    container config done

--- basics_1/conversion.js
+++ basics_1/conversion.js
@@ -5,11 +5,16 @@
 // console.log(typeof change);
 // console.log(change);
 
-let IndiaIsGreat = 1;
-console.log(typeof IndiaIsGreat);
+// let IndiaIsGreat = 1;
+// console.log(typeof IndiaIsGreat);
 
-let bool = Boolean(IndiaIsGreat);
-console.log(typeof bool)
-console.log(bool);
+// let bool = Boolean(IndiaIsGreat);
+// console.log(typeof bool)
+// console.log(bool);
 
+// let name1 = "chacha ";
+// let name2 = "chaudhary";
+// console.log(name1.concat(name2))
 
+console.log(3+4*5%3);
+console.log(5%3);
