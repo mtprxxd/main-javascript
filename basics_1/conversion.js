3a25fce64eeaecc7c0d12bc81125638a60a3cebe
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2025-02-05 18:00:43 +0000

    container config done

---
+++ basics_1/conversion.js
@@ -0,0 +1,15 @@
+// let IndiaIsGreat = false;
+// console.log(typeof IndiaIsGreat);
+
+// let change = Number(IndiaIsGreat);
+// console.log(typeof change);
+// console.log(change);
+
+let IndiaIsGreat = 1;
+console.log(typeof IndiaIsGreat);
+
+let bool = Boolean(IndiaIsGreat);
+console.log(typeof bool)
+console.log(bool);
+
+
