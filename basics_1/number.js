1cff2b406629307c9d19b19f70667bda6065088a
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2025-02-17 17:50:44 +0000

    container config done

--- basics_1/number.js
+++ basics_1/number.js
@@ -2,13 +2,25 @@
 // let score = 100;
 // console.log(score);
 
-let tata = new Number(10000);
-console.log(tata);
+// let tata = new Number(10000);
+// console.log(tata);
 
-let bye = new Number(100.786);
-console.log(tata.toFixed(4));
+// let bye = new Number(100.786);
+// console.log(tata.toFixed(4));
 
-console.log(tata.toLocaleString());
-console.log(bye.toPrecision(5));
-console.log(tata.toString(2));
+// console.log(tata.toLocaleString());
+// console.log(bye.toPrecision(5));
+// console.log(tata.toString(2));
 // console.log(typeof tata);
+
+
+// -----------------------Maths-------------------
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 11
+const max = 19
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
