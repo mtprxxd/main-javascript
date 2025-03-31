8ee6631c8922f65b0944d858128cf0b38dd80862
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2025-03-31 11:52:57 +0000

    container config done

--- iterations/for_Loop.js
+++ iterations/for_Loop.js
@@ -1,6 +1,38 @@
 // for Loop
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (let i = 0; i < 5; i++) {
+    const element = i;
+    console.log(element);
 }
+
+// // if-else in loops
+// for (let i = 0; i <= 5; i++) {
+//     const element = i;
+//     if (i==3) {
+//         console.log("3 is a best number")
+//     }
+//     console.log(element)
+// }
+
+
+// loop inside loop
+// for (let i = 0; i <= 5; i++) {
+//     console.log(`outer loop is : ${i}`)
+//     for (let j = 0; j<= 5; j++) {
+//         console.log(`inner loop is : ${j}  and Outer loop is : ${i} `)
+        
+//     }
+   
+// }
+
+
+// // print tables
+// for (let i = 0; i <= 5; i++) {
+//     // console.log(`outer loop is : ${i}`)
+//     for (let j = 0; j<= 5; j++) {
+//         // console.log(`inner loop is : ${j}  and Outer loop is : ${i} `)
+//         console.log(i + '*' + j + '=' + i*j);
+        
+//     }
+   
+// }
