5ded76affd92fe243ee8b9bc9a0fcdfa62cfe3ca
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2025-09-09 08:43:29 +0000

    container config done

--- Basic_Project02/script.js
+++ Basic_Project02/script.js
@@ -1,12 +1,27 @@
 const form = document.querySelector('form')
+const zone = document.querySelector('.range')
 
 form.addEventListener('submit',function(e){
-    e.defaultPrevented()
+    e.preventDefault()
 
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
-    const result = document.querySelector('results')
+    const result = document.querySelector('#results')
+    const zone = document.querySelector('.range')
 
-    if(height === ''|| height < 0 || isNaN(height))
+    if(height === ''|| height < 0 || isNaN(height)){
+        result.innerHTML = `<span>please enter the valid height ${height}</span>`
+    } else if(weight === ''|| weight < 0 || isNaN(weight)){
+        result.innerHTML = `<span>please enter the valid weight${weight}</span>`
+        // alert("Numbers Required only")
+    } else{
+        const bmi = (weight / ((height * height)/10000)).toFixed(2);
+        result.innerHTML = `<span>Calculated BMI ${bmi}</span>`
+        result.style.color = 'blue'
+        result.style.fontSize = '20px'
+    }
+     
+
+    
 
 })
