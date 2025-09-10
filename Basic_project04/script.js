9f2f5b49b1ee140bd5189d5390d181ebde85e098
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2025-09-10 17:23:47 +0000

    container config done

---
+++ Basic_project04/script.js
@@ -0,0 +1,25 @@
+const random = Math.floor((Math.random() * 100)+1)
+
+
+const submit = document.querySelector('#subt')
+const format = document.querySelector('#form')
+const result = document.querySelector('#results')
+
+
+format.addEventListener('submit',function(e){
+    e.preventDefault() 
+    
+    const userInput = parseInt(document.querySelector('#guessField').value)
+
+    if(userInput === '' || isNaN(userInput)){
+        result.innerHTML = `<span>Please enter a number</span>`
+    }else if(userInput === random) {
+        result.innerHTML = `<span style = "color: #00FF00; font-size: 16px; ">Congrats! ${userInput} is correct number</span>`
+    }else {
+        result.innerHTML = `<span style = "color: #FF0000; font-size: 16px;">No! ${userInput} doesn't match any guess</span>`
+    }
+})
+    
+
+
+
